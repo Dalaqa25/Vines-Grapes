f1a8285b149a0d76b1f362d18209bb030d73f1bf
dalaqa25 <g.dalaqishvili01@gmail.com>
2024-07-18 22:06:51 +0400

    First commit

---
+++ Models/GrapesModel.cs
@@ -0,0 +1,12 @@
+﻿
+namespace api.Models
+{
+    public class GrapesModel
+    {
+        public int Id {get; set;}
+        public string Name {get;set;} = string.Empty;
+        public DateTime CreatedOn {get; set;} = DateTime.Now;
+        public string Description {get;set;} = string.Empty;
+    }
+
+}
---
+++ Models/Rewievs.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Models
+{
+    public class Rewievs
+    {
+        public int Id {get;set;}
+        public string Rate {get;set;} = string.Empty;
+    }
+}
